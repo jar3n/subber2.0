0282b1f6dc389701de8bd12f0e987cd51b3f038d
jar3n <jamreng@proton.me>
2025-11-29 00:40:53 -0500

    fix comment typo

--- subber.py
+++ subber.py
@@ -130,10 +130,10 @@ def main():
             subs.set_sub_not_interested(handle)
     
     if args.play:
-        #  Desire is to have a way to vide youtube
+        # Desire is to have a way to provide youtube
         # videos without going to youtube website
-        # but youtube does not want that so
-        # i guess instead this command simply 
+        # but youtube does not want that
+        # instead this command simply 
         # opens the youtube video url
         # which is still a nice convenience
         sub_latest = subs.get_sub(args.play[0]).latest_video_link()
