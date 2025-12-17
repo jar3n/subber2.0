68a492cbd81a8d7a28ce2f16d1e60361c2d28a6f
jar3n <jamreng@proton.me>
2025-12-16 19:40:19 -0500

    moved api checking to its own module

--- subber.py
+++ subber.py
@@ -8,33 +8,11 @@
 # python modules
 import sys
 import argparse
-import configparser
-from os.path import dirname, abspath
-from pathlib import Path
-from inspect import getsourcefile
 import webbrowser
 
-
 # custom modules
+from api_key_utils import check_api_key, APIKeyRequestException
 from sublist import SubscriptionList
-
-
-def check_api_key():
-    """
-        Helper function to check for the api key configuration file
-    """
-    api_key_file = Path(f"{dirname(abspath(getsourcefile(lambda:0)))}/api_key")
-    if not api_key_file.is_file():
-        raise FileNotFoundError()
-
-    cp = configparser.ConfigParser()
-    try:
-        cp.read(str(api_key_file))
-    except configparser.MissingSectionHeaderError as e:
-        raise e
-
-    # return a subscription list object initialized with the key
-    return SubscriptionList(cp['key']['api key'])
 
 
 def subscribe(handles:list[str], subs:SubscriptionList):
@@ -176,16 +154,9 @@ def main():
     """
     subs = None
     try:
-        subs = check_api_key()
-    except FileNotFoundError:
-        print("ERROR: cannot find the api_key file with you " +
-        "youtube API key in it. Make sure the file is in the" +
-        " same directory as the subber.py file.")
-        return
-    except configparser.MissingSectionHeaderError:
-        print("ERROR: api key file not properly formatted."+
-            " Make sure there is a line with \'[key]\' followed "+
-            "by a line with \'api_key=YOUR_API_KEY\'")        
+        subs = SubscriptionList(check_api_key())
+    except APIKeyRequestException as e:
+        print(e)
         return
 
     parser = argparse.ArgumentParser(
--- ytclient_test.py
+++ ytclient_test.py
@@ -1,39 +1,26 @@
+"""
+    This module runs unit tests for the Youtube Client Class.
+    THis ensure that the class functions as expected and 
+    retrieves the information needed for each video and 
+    subscription.
+
+"""
+
 # Unit Testing for the youtube client class
-# so I don't have a headache of integration hell 
+# so I don't have a headache of integration hell
 # or at least I reduce it a little bit
 
-import configparser
-from os.path import dirname, abspath
-from pathlib import Path
-from inspect import getsourcefile
+
 from datetime import datetime
 import pytest
 
 from ytclient import YouTubeClient, YoutubeException
-
+from api_key_utils import check_api_key
 
 # ---------- Set up stuff ----------
 
-def check_api_key():
-    """
-        Helper function to check for the api key configuration file
-    """
-    api_key_file = Path(f"{dirname(abspath(getsourcefile(lambda:0)))}/api_key")
-    if not api_key_file.is_file():
-        raise FileNotFoundError()
-
-    cp = configparser.ConfigParser()
-    try:
-        cp.read(str(api_key_file))
-    except configparser.MissingSectionHeaderError as e:
-        raise e
-
-    # return a subscription list object initialized with the key
-    return cp['key']['api key']
-
-
 _API_KEY = check_api_key()
-Client = YouTubeClient(_API_KEY)
+CLIENT = YouTubeClient(_API_KEY)
 
 # API url endpoint
 API_URL = "https://www.googleapis.com/youtube/v3/"
@@ -67,13 +54,13 @@ def test_create_api_url():
     uploads_id = "q34234dsfqe3t4" # not a real uploads id
     suffix = f"playlistItems?part=snippet&playlistId={uploads_id}&maxResults=1"
     full_url = API_URL + suffix + f"&key={_API_KEY}"
-    assert full_url == Client.create_api_url(suffix)
+    assert full_url == CLIENT.create_api_url(suffix)
 
 def test_verify_handle():
     """Testing the verify channel handle
 	function can confirm 
     """
-    assert isinstance(Client.verify_handle(DUMMY_CHAN_HANDLE), str) is True
+    assert isinstance(CLIENT.verify_handle(DUMMY_CHAN_HANDLE), str) is True
 
 def test_verify_handle_fail():
     """Test the function raises an exception
@@ -81,7 +68,7 @@ def test_verify_handle_fail():
     to a channel
     """
     with pytest.raises(YoutubeException) as execinfo:
-        Client.verify_handle("jamesrenglander")
+        CLIENT.verify_handle("jamesrenglander")
 
     assert " is not linked to any channel" in str(execinfo)
 
@@ -89,13 +76,13 @@ def test_get_channel_id():
     """Testing getting the channel id
 	for a channel using the dummy channel
 	"""
-    assert DUMMY_CHAN_ID == Client.get_channel_id(DUMMY_CHAN_HANDLE)
+    assert DUMMY_CHAN_ID == CLIENT.get_channel_id(DUMMY_CHAN_HANDLE)
 
 def test_get_channel_details():
     """Testing getting the channel's
 	   display name and uploads id
 	"""
-    resps_disp_name, resp_ups_id = Client.get_channel_details(DUMMY_CHAN_ID)
+    resps_disp_name, resp_ups_id = CLIENT.get_channel_details(DUMMY_CHAN_ID)
 
     assert resp_ups_id == DUMMY_CHAN_UPLOADS_ID and resps_disp_name == DUMMY_CHAN_DISP_NAME
 
@@ -103,13 +90,13 @@ def test_get_duration():
     """Testing getting the duration 
        of a video from the video id
     """
-    assert DUMMY_CHAN_VID_DUR == Client.get_duration(DUMMY_CHAN_VID_ID)
+    assert DUMMY_CHAN_VID_DUR == CLIENT.get_duration(DUMMY_CHAN_VID_ID)
 
 def test_get_latest_upload():
     """Test getting the latest
        upload from the dummy channel
     """
-    title, pub_date, url, dur = Client.get_latest_upload(DUMMY_CHAN_UPLOADS_ID)
+    title, pub_date, url, dur = CLIENT.get_latest_upload(DUMMY_CHAN_UPLOADS_ID)
 
     assert title == DUMMY_CHAN_VID_NAME
     assert pub_date == DUMMY_CHAN_VID_PUB_DATE
